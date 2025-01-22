96b1c55c768cb29c46c52503e66f64d66716d8ad
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2025-01-21 21:10:17 -0300

    inicial commit

---
+++ src/main/java/com/example/GamesLists/GamesListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GamesLists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesListsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GamesLists/GamesListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GamesLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
